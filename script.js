20222781d63f9e90cb136b2778b30b22610c8267
School <kaboy@icloud.com>
2021-10-27 02:34:31 -0500

    add map, zoom controls, and fullscreen

---
+++ script.js
@@ -0,0 +1,22 @@
+// function displayRestaurantAround() {
+//   return new Promise((resolve, reject) => {
+//     this.service.nearbySearch({
+//       location: currentUserPosition,
+//       radius: 5000,
+//       types: ['restaurant']
+//     }, (res) => {
+//       resolve(res);
+//     });
+//   });
+// }
+
+// var request = {
+//   location: pyrmont,
+//   type: ['restaurant']
+// };
+
+// infowindow = new google.maps.InfoWindow();
+// places = new google.maps.places.PlacesService(map);
+// places.nearbySearch(request, callback);
+
+// console.log(infowindow.places);
